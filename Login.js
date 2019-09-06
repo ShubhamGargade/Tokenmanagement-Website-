bc6937be1b7fc1f1fb67a0a49f0906e753387b02
ShubhamGargade <46845226+ShubhamGargade@users.noreply.github.com>
2019-09-06 23:36:14 +0530

    Added additional feature for validation

--- Login.js
+++ Login.js
@@ -33,3 +33,29 @@ function validation() {
 function usertype(user){
   document.getElementById("hide").value = user;
 }
+
+// ------------------------------------------------------checking for empty-----------------------------------------------------------------
+
+function isEmpty1() {
+  var e = document.getElementById('username').value;
+  if(e.length > 0)
+  {
+    document.getElementById('user').innerHTML="";
+  }
+}
+
+function isEmpty2() {
+  var e = document.getElementById('password').value;
+  if(e.length > 0)
+  {
+    document.getElementById('pass').innerHTML="";
+  }
+}
+
+function isEmpty3() {
+  var e = document.getElementById('hide').value;
+  if(e.length > 0)
+  {
+    document.getElementById('whologin').innerHTML="";
+  }
+}
--- register.js
+++ register.js
@@ -22,47 +22,53 @@ function validation1(){
   console.log(srollno);
 
   if(!sname){
-    document.getElementById('sn').innerHTML="Username should not be empty";
+    document.getElementById('sn').innerHTML="*Username should not be empty";
     return false;
   }
 
   if(!semail){
-    document.getElementById('se').innerHTML="Email should not be empty";
+    document.getElementById('se').innerHTML="*Email should not be empty";
     return false;
   }
 
-  if(semail.length < 10 ){
-    document.getElementById('se').innerHTML="Email should not be atleast 10 character long";
+  if(semail.length < 12 ){
+    document.getElementById('se').innerHTML="*Email should be atleast 12 character long";
     return false;
   }
 
   if(!syear){
-    document.getElementById('sy').innerHTML="Select year";
+    document.getElementById('sy').innerHTML="*Select year";
     return false;
   }
 
   if(!sbranch){
-    document.getElementById('sb').innerHTML="Select branch";
+    document.getElementById('sb').innerHTML="*Select branch";
     return false;
   }
 
   if(!spassword){
-    document.getElementById('spass').innerHTML="Password should not be empty";
+    document.getElementById('spass').innerHTML="*Password should not be empty";
     return false;
   }
 
   if(spassword.length < 8){
-    document.getElementById('spass').innerHTML="Password should not be atleast 8 character long";
+    document.getElementById('spass').innerHTML="*Password should be atleast 8 character long";
     return false;
   }
 
   if(!srollno){
-    document.getElementById('srn').innerHTML="RollNo should not be empty";
+    document.getElementById('srn').innerHTML="*RollNo should not be empty";
     return false;
   }
 
   if(isNaN(srollno)){
-    document.getElementById('srn').innerHTML="RollNo should not be in No.";
+    document.getElementById('srn').innerHTML="*RollNo should be in Number";
+    return false;
+  }
+
+  if((srollno / 100) >= 1){
+
+    document.getElementById('srn').innerHTML="*RollNo should be atmost 2 character";
     return false;
   }
 
@@ -80,35 +86,124 @@ function validation2(){
   var tpassword  = document.getElementById('tPassword').value;
 
   if(!tname){
-    document.getElementById('tn').innerHTML="Username should not be empty";
+    document.getElementById('tn').innerHTML="*Username should not be empty";
     return false;
   }
 
   if(!temail){
-    document.getElementById('te').innerHTML="Email should not be empty";
+    document.getElementById('te').innerHTML="*Email should not be empty";
     return false;
   }
 
-  if(temail.length < 10 ){
-    document.getElementById('te').innerHTML="Email should not be atleast 10 character long";
+  if(temail.length < 12 ){
+    document.getElementById('te').innerHTML="*Email should be atleast 12 character long";
     return false;
   }
 
   if(!tbranch){
-    document.getElementById('tb').innerHTML="Select branch";
+    document.getElementById('tb').innerHTML="*Select branch";
     return false;
   }
 
   if(!tpassword){
-    document.getElementById('tpass').innerHTML="Password should not be empty";
+    document.getElementById('tpass').innerHTML="*Password should not be empty";
     return false;
   }
 
   if(tpassword.length < 8){
-    document.getElementById('tpass').innerHTML="Password should not be atleast 8 character long";
+    document.getElementById('tpass').innerHTML="*Password should be atleast 8 character long";
     return false;
   }
 
   alert("Successfully created an account");
 
+}
+
+
+
+
+// ------------------------------------------------------checking for empty (Student)-----------------------------------------------------------------
+
+function isEmpty1() {
+  var e = document.getElementById('sName').value;
+  if(e.length > 0)
+  {
+    document.getElementById('sn').innerHTML="";
+  }
+}
+
+function isEmpty2() {
+  var e = document.getElementById('sEmail').value;
+  if(e.length > 0)
+  {
+    document.getElementById('se').innerHTML="";
+  }
+}
+
+function isEmpty3() {
+  var e = document.getElementById('sYear').value;
+  if(e.length > 0)
+  {
+    document.getElementById('sy').innerHTML="";
+  }
+}
+
+function isEmpty4() {
+  var e = document.getElementById('sBranch').value;
+  if(e.length > 0)
+  {
+    document.getElementById('sb').innerHTML="";
+  }
+}
+
+function isEmpty5() {
+  var e = document.getElementById('sPassword').value;
+  if(e.length > 0)
+  {
+    document.getElementById('spass').innerHTML="";
+  }
+}
+
+function isEmpty6() {
+  var e = document.getElementById('sRollNo').value;
+  if(e.length > 0)
+  {
+    document.getElementById('srn').innerHTML="";
+  }
+}
+
+
+
+// ------------------------------------------------------checking for empty (Teacher)-----------------------------------------------------------------
+
+function isEmpty11() {
+  var e = document.getElementById('tName').value;
+  if(e.length > 0)
+  {
+    document.getElementById('tn').innerHTML="";
+  }
+}
+
+function isEmpty22() {
+  var e = document.getElementById('tEmail').value;
+  if(e.length > 0)
+  {
+    document.getElementById('te').innerHTML="";
+  }
+}
+
+function isEmpty33() {
+  var e = document.getElementById('tBranch').value;
+  if(e.length > 0)
+  {
+    document.getElementById('tb').innerHTML="";
+  }
+}
+
+function isEmpty44() {
+  var e = document.getElementById('tPassword').value;
+  if(e.length > 0)
+  {
+    document.getElementById('tpass').innerHTML="";
+  }
 }
